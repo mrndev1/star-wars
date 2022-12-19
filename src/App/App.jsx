e399618cfe7d4eb31e66822de28e16832ceddcde
mrndev1 <moranndev@gmail.com>
2022-12-19 18:17:50 -0300

    first

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -7,7 +7,7 @@ import Home from './pages/Home/Home';
 import FilmSection from './components/FilmSection/FilmSection';
 import CharacterSection from './components/CharacterSection copy/CharacterSection';
 import PlanetSection from './components/PlanetSection/PlanetSection';
-import LandingPage from './pages/LandingPage/LandingPage';
+
 
 export default function App() {
     return routes;
@@ -15,7 +15,6 @@ export default function App() {
 
 const routes = (
     <Routes>
-        <Route path='/' element={<LandingPage/>}/>
         <Route exact path='/home' element={<Home/>}>
             <Route path='/home/films' element={<FilmSection/>}/>
             <Route path='/home/characters' element={<CharacterSection/>}/>
